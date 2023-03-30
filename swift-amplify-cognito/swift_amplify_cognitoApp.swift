b1bf7113fa1c92c1b9ed0ccb1f57e532af2b9d06
Ross Gill <ross.gill.10@gmail.com>
2023-03-30 13:09:12 -0500

    Initial Commit

---
+++ swift-amplify-cognito/swift_amplify_cognitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_amplify_cognitoApp.swift
+//  swift-amplify-cognito
+//
+//  Created by Ross Gill on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_amplify_cognitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
